4919c5db1c59be07944577e90877f69e7ea7f561
Nevac14 <43809134+NevinKoshyDaniel@users.noreply.github.com>
2024-07-06 13:02:28 +0530

    Config Updates

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import { join } from 'path'
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin'
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'selector',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		typography,
+		forms,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'crimson',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+};
